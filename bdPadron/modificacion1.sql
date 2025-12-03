f652bbc930cff970642c4fea1acdb16d84fc1863
J Ignacio M <jmonsar029@g.educaand.es>
2025-12-03 16:49:50 +0100

    modificaciones SQL

---
+++ bdPadron/modificacion1.sql
@@ -0,0 +1,13 @@
+USE bdPadron;
+
+UPDATE taProvincias SET vaNomProvincia = "Álava" WHERE vaNomProvincia LIKE 'Araba%';
+UPDATE taProvincias SET vaNomProvincia = "Alicante" WHERE vaNomProvincia LIKE 'Alicante%';
+UPDATE taProvincias SET vaNomProvincia = "Islas Baleares" WHERE vaNomProvincia LIKE 'Balears%';
+UPDATE taProvincias SET vaNomProvincia = "Castellón" WHERE vaNomProvincia LIKE 'Castell%';
+UPDATE taProvincias SET vaNomProvincia = "Coruña, La" WHERE vaNomProvincia LIKE 'Coruña%';
+UPDATE taProvincias SET vaNomProvincia = "Gerona" WHERE vaNomProvincia LIKE 'Girona';
+UPDATE taProvincias SET vaNomProvincia = "Guipúzcoa" WHERE vaNomProvincia LIKE 'Gipuzk%';
+UPDATE taProvincias SET vaNomProvincia = "Lérida" WHERE vaNomProvincia LIKE 'Lleida';
+UPDATE taProvincias SET vaNomProvincia = "Orense" WHERE vaNomProvincia LIKE 'Oure%';
+UPDATE taProvincias SET vaNomProvincia = "Valencia" WHERE vaNomProvincia LIKE 'Valencia%';
+UPDATE taProvincias SET vaNomProvincia = "Vizcaya" WHERE vaNomProvincia LIKE 'Bizkaia';
---
+++ bdPadron/modificacion3.sql
@@ -0,0 +1,10 @@
+USE bdPadron;
+
+CREATE TABLE IF NOT EXISTS taComunidades (
+    iCodComunidad INT NOT NULL,
+    vaNomComunidad varchar(30) NOT NULL,
+    PRIMARY KEY (iCodComunidad)
+);
+
+
+
