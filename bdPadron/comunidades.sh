c64ab7b2065a6721501f9eb757f936ca2e182680
J Ignacio M <jmonsar029@g.educaand.es>
2025-12-05 21:39:41 +0100

    Terminada la modificacion 1

--- bdPadron/comunidades.sh
+++ bdPadron/comunidades.sh
@@ -1,21 +1,12 @@
 #!/bin/bash
 
+if [[ ! -f comunidades_autonomas_ref.txt ]];then
+    echo "No existe el fichero"
+    exit 1
+fi
 
-echo -e "USE bdPadron;\n"
-echo -e "CREATE TABLE IF NOT EXISTS taComunidades (
-    iCodComunidad int NOT NULL,
-    vaNomComunidad varchar(50),
-    PRIMARY KEY iCodComunidad
-);\n"
 
 while IFS=\; read cod comunidad;do
-    echo "INSERT INTO taComunidades VALUES ($cod, \"$comunidad\");"
+    echo "INSERT INTO taComunidades (vaNomComunidad) VALUES (\"$comunidad\");"
 done < comunidades_autonomas_ref.txt
 
-
-echo -e "\nALTER TABLE taProvincias
-ADD COLUMN iRefComunidad int NOT NULL"
-
-
-
-exit 0
