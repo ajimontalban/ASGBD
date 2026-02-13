f61abc993afed9ff23cb465b66cc293eb9913541
J. Ignacio Montalbán <jmonsar029@g.educaand.es>
2026-02-13 21:40:52 +0100

    Corregidos los procedimiento 1 y 3

--- bdPadron/R3/procedimiento1.sql
+++ bdPadron/R3/procedimiento1.sql
@@ -1,3 +1,4 @@
+-- Metodo 1
 DROP PROCEDURE IF EXISTS prComunidadPoblacionMinima;
 DELIMITER //
 CREATE PROCEDURE prComunidadPoblacionMinima(OUT _vaNomComunidadProvinciaMin VARCHAR(30))
@@ -19,6 +20,121 @@ BEGIN
     OPEN curComunidades;
 
     FETCH curComunidades INTO _iCodComunidad;
+    WHILE _iFinCursor = 0 DO
+        SET _sumaprovincia = (
+        select sum(iPoblacion) as sumaprovincia
+        from taPoblacion,taMunicipios,taProvincias
+        where iRefMunicipio = iCodMunicipio 
+        and   iRefProvincia = iCodProvincia
+        and iAnio = 2024 and iRefComunidad = _iCodComunidad
+        group by iCodProvincia
+        order by sumaprovincia asc
+        limit 1);
+        IF _sumaprovincia < _iPoblacionMin THEN
+            SET _iPoblacionMin = _sumaprovincia;
+            SET _iCodComunidadMin = _iCodComunidad;
+        END IF;
+        FETCH curComunidades INTO _iCodComunidad;
+    END WHILE;
+
+    SELECT vaNomComunidad into _vaNomComunidadProvinciaMin
+    FROM taComunidades
+    WHERE iCodComunidad = _iCodComunidadMin;
+
+    CLOSE curComunidades;
+END //
+DELIMITER ;
+
+-- Metodo 2
+DROP PROCEDURE IF EXISTS prComunidadPoblacionMinima;
+DELIMITER //
+CREATE PROCEDURE prComunidadPoblacionMinima(OUT _vaNomComunidadProvinciaMin VARCHAR(30))
+BEGIN
+    DECLARE _iCodComunidad INT;
+    DECLARE _iFinCursor INT;
+    DECLARE _sumaprovincia INT;
+    DECLARE _iPoblacionMin, _iCodComunidadMin INT;
+
+    DECLARE curComunidades CURSOR FOR
+    SELECT iCodComunidad from taComunidades 
+    WHERE iCodComunidad != 18 AND iCodComunidad != 19;
+
+    DECLARE CONTINUE HANDLER FOR NOT FOUND SET _iFinCursor = 1;
+
+    SET _iFinCursor = 0;
+    SET _iPoblacionMin = (
+        select sum(iPoblacion) as sumaprovincia
+        from taPoblacion,taMunicipios,taProvincias
+        where iRefMunicipio = iCodMunicipio 
+        and   iRefProvincia = iCodProvincia
+        and iAnio = 2024 and iRefComunidad = 1
+        group by iCodProvincia
+        order by sumaprovincia asc
+        limit 1);
+    SET _iCodComunidadMin = 1;
+
+    OPEN curComunidades;
+
+    FETCH curComunidades INTO _iCodComunidad;
+    WHILE _iFinCursor = 0 DO
+        SET _sumaprovincia = (
+        select sum(iPoblacion) as sumaprovincia
+        from taPoblacion,taMunicipios,taProvincias
+        where iRefMunicipio = iCodMunicipio 
+        and   iRefProvincia = iCodProvincia
+        and iAnio = 2024 and iRefComunidad = _iCodComunidad
+        group by iCodProvincia
+        order by sumaprovincia asc
+        limit 1);
+        IF _sumaprovincia < _iPoblacionMin THEN
+            SET _iPoblacionMin = _sumaprovincia;
+            SET _iCodComunidadMin = _iCodComunidad;
+        END IF;
+        FETCH curComunidades INTO _iCodComunidad;
+    END WHILE;
+
+    SELECT vaNomComunidad into _vaNomComunidadProvinciaMin
+    FROM taComunidades
+    WHERE iCodComunidad = _iCodComunidadMin;
+
+    CLOSE curComunidades;
+END //
+DELIMITER ;
+
+-- Metodo 3
+DROP PROCEDURE IF EXISTS prComunidadPoblacionMinima;
+DELIMITER //
+CREATE PROCEDURE prComunidadPoblacionMinima(OUT _vaNomComunidadProvinciaMin VARCHAR(30))
+BEGIN
+    DECLARE _iCodComunidad INT;
+    DECLARE _iFinCursor INT;
+    DECLARE _sumaprovincia INT;
+    DECLARE _iPoblacionMin, _iCodComunidadMin INT;
+
+    DECLARE curComunidades CURSOR FOR
+    SELECT iCodComunidad from taComunidades 
+    WHERE iCodComunidad != 18 AND iCodComunidad != 19;
+
+    DECLARE CONTINUE HANDLER FOR NOT FOUND SET _iFinCursor = 1;
+
+    SET _iFinCursor = 0;
+
+    OPEN curComunidades;
+
+    FETCH curComunidades INTO _iCodComunidad;
+
+    SET _iPoblacionMin = (
+        select sum(iPoblacion) as sumaprovincia
+        from taPoblacion,taMunicipios,taProvincias
+        where iRefMunicipio = iCodMunicipio 
+        and   iRefProvincia = iCodProvincia
+        and iAnio = 2024 and iRefComunidad = _iCodComunidad
+        group by iCodProvincia
+        order by sumaprovincia asc
+        limit 1);
+
+    SET _iCodComunidadMin = _iCodComunidad;
+
     WHILE _iFinCursor = 0 DO
         SET _sumaprovincia = (
         select sum(iPoblacion) as sumaprovincia
--- bdPadron/R3/procedimiento3.sql
+++ bdPadron/R3/procedimiento3.sql
@@ -8,10 +8,38 @@ BEGIN
     DECLARE _iCodComunidad INT;
     DECLARE _iCodComunidadMax INT;
 
-    CREATE TEMPORARY TABLE tabla_temporal (
-        _comunidad VARCHAR(80);
-        _municipios  INT;
-    );
+    DECLARE curComunidades CURSOR FOR
+        SELECT iCodComunidad FROM taComunidades;
+
+    DECLARE CONTINUE HANDLER FOR NOT FOUND SET _iFinCursor = 1;
+
+    SET _iFinCursor = 0;
+    SET _municipiosmax = -1;
+
+    OPEN curComunidades;
+    FETCH curComunidades INTO _iCodComunidad;
+    WHILE _iFinCursor = 0 DO
+        SELECT _sumamunicipios := count(*) FROM taMunicipios, taProvincias
+        WHERE iRefProvincia=iCodProvincia AND iRefComunidad = _iCodComunidad;
+        IF _sumamunicipios > _municipiosmax THEN
+            SET _municipiosmax = _sumamunicipios;
+            SET _iCodComunidadMax = _iCodComunidad;
+        END IF;
+        FETCH curComunidades INTO _iCodComunidad;
+    END WHILE;
+
+END //
+DELIMITER ;
+
+DROP PROCEDURE IF EXISTS prComunidadesMasMunicipios;
+DELIMITER //
+CREATE PROCEDURE prComunidadesMasMunicipios()
+BEGIN
+    DECLARE _iFinCursor INT;
+    DECLARE _sumamunicipios INT;
+    DECLARE _municipiosmax INT;
+    DECLARE _iCodComunidad INT;
+    DECLARE _iCodComunidadMax INT;
 
     DECLARE curComunidades CURSOR FOR
         SELECT iCodComunidad FROM taComunidades;
