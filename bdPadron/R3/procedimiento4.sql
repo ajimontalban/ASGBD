4e689c907808f315d8209d6409ebcf4d9940ae01
J. Ignacio Montalbán <jmonsar029@g.educaand.es>
2026-02-18 16:42:21 +0100

    Realizado el procedimiento 4

--- bdPadron/R3/procedimiento4.sql
+++ bdPadron/R3/procedimiento4.sql
@@ -2,21 +2,23 @@ DROP PROCEDURE IF EXISTS prProvinciaMasHombres;
 DELIMITER //
 CREATE PROCEDURE prProvinciaMasHombres()
 BEGIN
-    CREATE TEMPORARY TABLE test (
-    
-        NombreProvincia VARCHAR(40),
-        PobHombres DECIMAL(14,2),
-        PobMujeres DECIMAL(14,2)
-    )
 
-    DECLARE _iFinCursor,_iCodProvincia INT;
+    DECLARE _iFinCursor INT;
+    DECLARE _iCodProvincia INT;
     DECLARE _dHombres, _dMujeres DECIMAL(14,2);
     DECLARE _vaNomProvincia VARCHAR(40);
 
     DECLARE curProvincias CURSOR FOR
-        SELECT iCodProvincias FROM taProvincias;
+        SELECT iCodProvincia FROM taProvincias;
 
     DECLARE CONTINUE HANDLER FOR NOT FOUND SET _iFinCursor = 1;
+
+    CREATE TEMPORARY TABLE IF NOT EXISTS taSalida (
+ 
+        NombreProvincia VARCHAR(40),
+        PobHombres DECIMAL(14,2),
+        PobMujeres DECIMAL(14,2)
+    );
 
     SET _iFinCursor = 0;
     
@@ -24,25 +26,26 @@ BEGIN
 
     FETCH curProvincias INTO _iCodProvincia;
     WHILE _iFinCursor = 0 DO
-        select _dHombres:= dPoblacion FROM taPoblacionEdadesProvincias 
-        WHERE iRefProvincia = _iCodProvincia AND iAnio = 2024 
+        select  sum(dPoblacion) INTO _dHombres FROM taPoblacionEdadesProvincias 
+        WHERE iRefProvincia = _iCodProvincia AND iAnio = 2022 
         AND iPeriodo = 2 AND cSexo = 'H' AND iEdad >= 18;
 
-        select _dMujeres:= dPoblacion FROM taPoblacionEdadesProvincias 
-        WHERE iRefProvincia = _iCodProvincia AND iAnio = 2024 
+        select  sum(dPoblacion) INTO _dMujeres FROM taPoblacionEdadesProvincias 
+        WHERE iRefProvincia = _iCodProvincia AND iAnio = 2022 
         AND iPeriodo = 2 AND cSexo = 'M' AND iEdad >= 18;
 
-        IF(_dHombres > _dMujeres) THEN
+        IF _dHombres > _dMujeres THEN
             SET _vaNomProvincia = (SELECT vaNomProvincia FROM taProvincias
             WHERE iCodProvincia = _iCodProvincia);
-        INSERT INTO test (_vaNomProvincia, _dHombres, _dMujeres);
+            INSERT INTO taSalida (NombreProvincia, PobHombres, PobMujeres)
+            VALUES (_vaNomProvincia, _dHombres, _dMujeres);
         END IF;
         FETCH curProvincias INTO _iCodProvincia;
     END WHILE;
 
-    SELECT test;
+    SELECT *  FROM taSalida;
 
     CLOSE curProvincias;
-    DROP test;
+    DROP TEMPORARY TABLE taSalida;
 END //
 DELIMITER ;
